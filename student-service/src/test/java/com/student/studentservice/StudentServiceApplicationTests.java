a442d054bf6d1ee790f2aed2be2266ea3adf5970
sovisrushain <sovisrushain@gmail.com>
2023-02-25 13:43:19 +0530

    initialize student service

---
+++ student-service/src/test/java/com/student/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
