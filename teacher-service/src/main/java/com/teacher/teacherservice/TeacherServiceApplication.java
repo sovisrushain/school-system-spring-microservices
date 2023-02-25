888b6a79fbdc6de191ba9fcc4467871ec21052de
sovisrushain <sovisrushain@gmail.com>
2023-02-25 13:48:46 +0530

    initialize teacher service

---
+++ teacher-service/src/main/java/com/teacher/teacherservice/TeacherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teacher.teacherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherServiceApplication.class, args);
+	}
+
+}
---
+++ teacher-service/src/test/java/com/teacher/teacherservice/TeacherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacher.teacherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
