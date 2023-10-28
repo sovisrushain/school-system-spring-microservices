d1d221c34a4447da1fc175460ae10228be696eae
sovisrushain <sovisrushain@gmail.com>
2023-10-28 14:51:00 +0530

    impl of logs | student service

--- student-service/src/main/java/com/student/studentservice/controller/StudentController.java
+++ student-service/src/main/java/com/student/studentservice/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.student.studentservice.controller;
 import com.student.studentservice.dto.StudentDTO;
 import com.student.studentservice.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,23 +16,29 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    private static final Logger logger = LogManager.getLogger(StudentController.class);
+
     @PostMapping
     public String saveStudent(@RequestBody StudentDTO studentDTO) {
+        logger.info("StudentController.class: saveStudent(): start");
         return studentService.saveStudent(studentDTO);
     }
 
     @GetMapping("/{studentId}")
     public StudentDTO getStudentById(@PathVariable String studentId) {
+        logger.info("StudentController.class: getStudentById(): start");
         return studentService.getStudentById(studentId);
     }
 
     @GetMapping
     public List<StudentDTO> findAllStudents() {
+        logger.info("StudentController.class: findAllStudents(): start");
         return studentService.findAllStudents();
     }
 
     @DeleteMapping("/{studentId}")
     public String deleteStudentById(@PathVariable String studentId) {
+        logger.info("StudentController.class: deleteStudentById(): start");
         return studentService.deleteStudentById(studentId);
     }
 }
--- student-service/src/main/java/com/student/studentservice/service/StudentServiceImpl.java
+++ student-service/src/main/java/com/student/studentservice/service/StudentServiceImpl.java
@@ -4,6 +4,8 @@ import com.student.studentservice.dto.StudentDTO;
 import com.student.studentservice.model.StudentDAO;
 import com.student.studentservice.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +17,11 @@ public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
 
+    private static final Logger logger = LogManager.getLogger(StudentServiceImpl.class);
+
     @Override
     public String saveStudent(StudentDTO studentDTO) {
+        logger.info("StudentServiceImpl.class: saveStudent(): start");
         StudentDAO student = StudentDAO.builder()
                 .studentId(studentDTO.getStudentId())
                 .teacherId(studentDTO.getTeacherId())
@@ -28,6 +33,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public StudentDTO getStudentById(String studentId) {
+        logger.info("StudentServiceImpl.class: getStudentById(): start");
         Optional<StudentDAO> student = studentRepository.findById(studentId);
         return StudentDTO.builder()
                 .studentId(student.get().getStudentId())
@@ -38,6 +44,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<StudentDTO> findAllStudents() {
+        logger.info("StudentServiceImpl.class: findAllStudents(): start");
         List<StudentDAO> studentList = studentRepository.findAll();
         return studentList.stream().map(student -> StudentDTO.builder().
                 studentId(student.getStudentId())
@@ -49,6 +56,7 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public String deleteStudentById(String studentId) {
+        logger.info("StudentServiceImpl.class: deleteStudentById(): start");
         studentRepository.deleteById(studentId);
         return studentId;
     }
